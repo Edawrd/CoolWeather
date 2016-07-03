d195340216d1b8eec6df2e4de0bee116b53e84df
Edawrd <591931394@qq.com>
2016-07-03 15:22:43 +0800

    2

---
+++ src/main/java/model/City.java
@@ -0,0 +1,43 @@
+package model;
+
+/**
+ * Created by Edward on 2016/6/30.
+ */
+public class City {
+    private int cityId;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
